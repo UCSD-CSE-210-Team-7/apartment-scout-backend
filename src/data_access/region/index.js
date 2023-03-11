d065ae402152e72ddd5c29ab1acd484b228db2f9
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2023-03-11 01:55:33 -0800

    exclude any users from regions query that are not currently set to is_scout=true

--- src/data_access/region/index.js
+++ src/data_access/region/index.js
@@ -1,14 +1,14 @@
 const { client } = require('../../utils/db.js')
 
 async function getUsers(region){
-    const query = 'SELECT email FROM UserRegion WHERE zipcode=$1'
+    const query = 'SELECT email from USERRegion JOIN Users USING (email) WHERE IS_SCOUT=TRUE AND ZIPCODE=$1'
     const args =  [region]
     const res = await client.query(query, args)
     return res.rows
 }
 
 async function getRegions(user){
-    const query = 'SELECT zipcode FROM UserRegion WHERE email=$1'
+    const query = 'SELECT zipcode from UserRegion JOIN Users USING (email) WHERE IS_SCOUT=TRUE AND email=$1'
     const args =  [user]
     const res = await client.query(query, args)
     return res.rows
